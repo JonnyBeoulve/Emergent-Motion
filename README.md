d95d954dedc88bf874da2f185b32c4081fe5c700
JonnyBeoulve <jleack@gmail.com>
2018-12-28 11:36:53 -0800

    Built About Page.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Emergent-Motion
-Using Gatsby.js, this project will use React, GraphQL, and more to present helpful physical therapy related health information to the user in blog-style format.
+Built with Gatsby.js, this project harnesses React and GraphQL to present helpful physical therapy related health information to the user in blog-style format.
 
 ## How to Use
 This project is currently in development. An alpha will be deployed to Firebase in the near future.
---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const AboutPage = () => {
+    return (
+        <Layout>
+            <SEO title="Page two" />
+            <h1>About</h1>
+            <p>Built by Full Stack Developer Jonathan Leack, Emergent Motion is a website that will showcase the power of Gatsby.js, React, and GraphQL. Here you will find physical therapy health information to help you live better.</p>
+            <Link to="/">Go back to the homepage</Link>
+        </Layout>
+
+  )
+}
+
+export default AboutPage
