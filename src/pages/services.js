114b7e14849004780e11b1033e79d64713ab2c25
JonnyBeoulve <jleack@gmail.com>
2018-12-28 11:42:40 -0800

    Built Services Page.

---
+++ src/pages/services.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const ServicesPage = () => {
+    return (
+        <Layout>
+            <SEO title="Services page" />
+            <h1>Services</h1>
+            <p>Life is too short to live with pain. All you want is a solution. We have one.</p>
+            <ul>
+                <li>Running</li>
+                <li>Dance</li>
+                <li>Gymnastics</li>
+                <li>Fitness and Wellness</li>
+                <li>Orthopedics</li>
+            </ul>
+            <Link to="/">Go back to the homepage</Link>
+        </Layout>
+
+  )
+}
+
+export default ServicesPage
