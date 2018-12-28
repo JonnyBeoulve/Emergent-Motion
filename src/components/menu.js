603b4bf7e22205af607e581dfd2d4a6fc232c7b8
JonnyBeoulve <jleack@gmail.com>
2018-12-28 12:14:53 -0800

    Menu styling update.

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,23 +2,28 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const Menu = () => {
-  return (
-      <div style={{
-          background: '#f3f3f3',
-          paddingTop: '10px'
-      }}>
-          <ul style={{
-              listStyle: 'none',
-              display: 'flex',
-              justifyContent: 'space-evenly'
-          }}>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/about">About</Link></li>
-              <li><Link to="/services">Services</Link></li>
-              <li><Link to="/">Coming Soon</Link></li>
-          </ul>
-      
-    </div>
+    return (
+        <div style={{
+            background: '#f3f3f3',
+            paddingTop: '10px'
+        }}>
+            <div style={{
+                margin: `0 auto`,
+                maxWidth: 960,
+                paddingTop: `0.6rem`
+            }}>
+                <ul style={{
+                    listStyle: 'none',
+                    display: 'flex',
+                    justifyContent: 'space-between'
+                }}>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/services">Services</Link></li>
+                    <li><Link to="/">Coming Soon</Link></li>
+                </ul>
+            </div>
+        </div>
   )
 }
 
